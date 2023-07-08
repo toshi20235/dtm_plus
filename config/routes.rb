e972899ca3d056503d96d62ec537ca77ffa8551c
toshi20235 <sisohanouta@gmail.com>
2023-07-08 09:39:51 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,51 @@
+Rails.application.routes.draw do
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  # root to: 'homes#top'
+  
+  namespace :admin do
+     get '/' => "homes#top"
+  end
+  
+  namespace :public do
+    
+    root to: "homes#top"
+    
+    # resources :posts do  #postsコントローラへのルーティング  
+    #   resources :comments, only: [:create]  #commentsコントローラへのルーティング
+    #   resource :favorites, only: [:create, :destroy]
+    # end
+    
+    resources :albums, only: [:index, :destroy]
+   
+    resources :users do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
+    
+    resources :musics, only: [:new, :index,:show,:create,:destroy] do
+      resources :comments, only: [:create]  #commentsコントローラへのルーティング
+      resource :favorites, only: [:create, :destroy]
+    end
+    
+    get 'users/my_page' => 'users#show'                          #顧客のマイページ.
+    get 'users/information/edit' => 'users#edit'                 #顧客の登録情報編集画面.
+    patch 'users/information' => 'users#update'                  #顧客の登録情報更新.
+    patch 'users/withdraw' => 'users#withdraw'        #顧客の退会処理(ステータスの更新).
+    
+  end
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: "public/homes#top"
+end
---
+++ db/migrate/20230705081123_create_musics.rb
@@ -0,0 +1,11 @@
+class CreateMusics < ActiveRecord::Migration[6.1]
+  def change
+    create_table :musics do |t|
+       t.integer :user_id, null: false
+       t.string :name, null: false
+       t.string :explanation, null: false
+       t.string :audio, null: false
+      t.timestamps
+    end
+  end
+end
