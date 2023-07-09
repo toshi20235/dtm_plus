aa6d9fe4ff52cbe04a59e18ecae8e6f7d399b742
toshi20235 <sisohanouta@gmail.com>
2023-07-09 03:01:18 +0000

    07/09/12:01

--- app/controllers/public/musics_controller.rb
+++ app/controllers/public/musics_controller.rb
@@ -1,5 +1,6 @@
 class Public::MusicsController < ApplicationController
   def new
+    @music = Music.new
   end
 
   def index
@@ -9,8 +10,18 @@ class Public::MusicsController < ApplicationController
   end
 
   def create
+    music = Music.new(music_params)
+    music.save
+    redirect_to users_my_page_path
   end
 
   def destroy
   end
+  
+  private
+  
+  def music_params
+    params.require(:music).permit(:name, :explanation, :audio, :post_content, :image)
+  end
+  
 end
--- app/models/music.rb
+++ app/models/music.rb
@@ -4,6 +4,7 @@ class Music < ApplicationRecord
   belongs_to :album
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  mount_uploader :audio, AudioUploader
   def favorited?(user)
    favorites.where(user_id: user.id).exists?
   end
