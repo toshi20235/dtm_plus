0b06c71108b117a1b776f8201a0b4f62d72f9290
toshi20235 <sisohanouta@gmail.com>
2023-07-10 09:20:37 +0000

    07/10/18:20

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,13 +1,29 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to users_my_page_path(@users)
   end
 
   def withdraw
+    @user = current_user
+    @user.update(withdrawal: true)
+    reset_session
+    redirect_to root_path
   end
+  
+   private
+  
+  def user_params
+    params.require(:user).permit(:user_name, :withdrawal, :image)
+  end
+  
 end
